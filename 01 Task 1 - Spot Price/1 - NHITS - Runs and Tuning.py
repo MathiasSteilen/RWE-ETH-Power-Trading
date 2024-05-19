6b1e96e9ad93f8e6cea7768de9b518dadc5b27ed
MathiasSteilen <mathias.steilen@gmail.com>
2024-05-20 00:57:37 +0200

    retrained LightGBM for swiss day ahead

--- 01 Task 1 - Spot Price/1 - NHITS - Runs and Tuning.py
+++ 01 Task 1 - Spot Price/1 - NHITS - Runs and Tuning.py
@@ -24,37 +24,35 @@ from sklearn.preprocessing import (
     OneHotEncoder,
     FunctionTransformer,
 )
-from sklearn.metrics import (
-    r2_score,
-    mean_absolute_error,
-    mean_absolute_percentage_error,
-    mean_squared_error,
-)
+from sklearn.metrics import r2_score, mean_absolute_error, mean_absolute_percentage_error, mean_squared_error
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn import config_context
 import optuna
 import os
 import holidays
+### Data Preparation
 
 df = pd.read_csv(
     "../00 Data Retrieval and Cleaning/0_df_final_ml_predictive.csv",
     parse_dates=["date"],
 )
 
-df = df.assign(
-    day_ahead_price_de=lambda x: x["day_ahead_price_de"].shift(24),
-    auction_price_de_ch=lambda x: x["auction_price_de_ch"].shift(24),
-    auction_price_ch_de=lambda x: x["auction_price_ch_de"].shift(24),
+df = (
+    df
+    .assign(
+        day_ahead_price_de=lambda x: x["day_ahead_price_de"].shift(24),
+        auction_price_de_ch=lambda x: x["auction_price_de_ch"].shift(24),
+        auction_price_ch_de=lambda x: x["auction_price_ch_de"].shift(24),
+    )
 )
 
 df = df.assign(trend=lambda x: x.index, unique_id="spot_ch")
-
-
 # Define the country (Switzerland)
 country = "CH"
 
-regional_holidays = holidays.CH(years=df.date.dt.year.unique().tolist())
-
+regional_holidays = holidays.CH(
+    years=df.date.dt.year.unique().tolist()
+)
 holiday_df = pd.DataFrame(
     {
         "holiday_name": list(regional_holidays.values()),
@@ -88,14 +86,12 @@ df = (
     )
 )
 
-
 def sin_transformer(period):
     return FunctionTransformer(lambda x: np.sin(x / period * 2 * np.pi))
 
 
 def cos_transformer(period):
     return FunctionTransformer(lambda x: np.cos(x / period * 2 * np.pi))
-
 
 # hour in day
 df["hour_sin"] = sin_transformer(24).fit_transform(df["hour"].astype(float))
@@ -118,12 +114,14 @@ df["wday_sin"] = sin_transformer(7).fit_transform(df["wday"].astype(float))
 df["wday_cos"] = cos_transformer(7).fit_transform(df["wday"].astype(float))
 
 df = df.drop(["hour", "month", "quarter", "wday", "week_hour"], axis=1)
-
+#### `sklearn` Pipeline for Data Preparation
 manual_cols = ["trend", "unique_id"]
 drop_cols = ["date", "day_ahead_price_ch"]
 
 pipeline_cols = [
-    col for col in df.drop(drop_cols, axis=1).columns if col not in manual_cols
+    col
+    for col in df.drop(drop_cols, axis=1).columns
+    if col not in manual_cols
 ]
 
 num_cols = (
@@ -142,7 +140,7 @@ cat_cols = (
 numeric_transformer = Pipeline(
     steps=[
         ("imputer", SimpleImputer()),
-        #    ("scaler", StandardScaler())
+        # ("scaler", StandardScaler())
     ]
 )
 
@@ -164,38 +162,47 @@ preprocessor = ColumnTransformer(
     ],
     remainder="passthrough",
 )
-
-
+### Preprocess the data
 train_end = pd.Timestamp("2023-09-01").tz_localize("UTC")
 val_end = pd.Timestamp("2024-01-01").tz_localize("UTC")
 
 # Create splits
-df_train = df.query("date < @val_end")
-# df_val = df.query("date < @val_end").query("ds >= @train_end").head(500)
-df_test = df.query("date >= @val_end")
+df_train = df.query("date < @train_end").reset_index(drop=True)
+df_val = df.query("date < @val_end").query("date >= @train_end").reset_index(drop=True)
+df_test = df.query("date >= @val_end").reset_index(drop=True)
+
 X_train = df_train.drop(columns=["date", "day_ahead_price_ch"])
 y_train = df_train["day_ahead_price_ch"]
 
+X_val = df_val.drop(columns=["date", "day_ahead_price_ch"])
+y_val = df_val["day_ahead_price_ch"]
+
 X_test = df_test.drop(columns=["date", "day_ahead_price_ch"])
 y_test = df_test["day_ahead_price_ch"]
+
 fitted_preprocessor = preprocessor.fit(X_train)
+
 X_train_preprocessed = pd.DataFrame(
     fitted_preprocessor.transform(X_train),
     columns=fitted_preprocessor.get_feature_names_out(),
 )
+
+X_val_preprocessed = pd.DataFrame(
+    fitted_preprocessor.transform(X_val),
+    columns=fitted_preprocessor.get_feature_names_out(),
+)
+
 X_test_preprocessed = pd.DataFrame(
     fitted_preprocessor.transform(X_test),
     columns=fitted_preprocessor.get_feature_names_out(),
 )
+
 # Replace prefixes in column names
-new_cols = (
-    X_train_preprocessed.columns.str.replace("numeric__", "")
-    .str.replace("categorical__", "")
-    .str.replace("remainder__", "")
-)
+new_cols = X_train_preprocessed.columns.str.replace('numeric__', '').str.replace('categorical__', '').str.replace('remainder__', '')
 
 # Assign new column names to the DataFrame
 X_train_preprocessed.columns = new_cols
+X_val_preprocessed.columns = new_cols
 X_test_preprocessed.columns = new_cols
 
 df_train = pd.concat(
@@ -203,6 +210,15 @@ df_train = pd.concat(
         df_train["date"].reset_index(drop=True),
         y_train.reset_index(drop=True),
         X_train_preprocessed.reset_index(drop=True),
+    ],
+    axis=1,
+)
+
+df_val = pd.concat(
+    [
+        df_val["date"].reset_index(drop=True),
+        y_val.reset_index(drop=True),
+        X_val_preprocessed.reset_index(drop=True),
     ],
     axis=1,
 )
@@ -216,30 +232,28 @@ df_test = pd.concat(
     axis=1,
 )
 
-# Rename to NeuralForecast column convention
+### Training an initial model for testing purposes
 df_train = df_train.rename(columns={"day_ahead_price_ch": "y", "date": "ds"})
+df_val = df_val.rename(columns={"day_ahead_price_ch": "y", "date": "ds"})
 df_test = df_test.rename(columns={"day_ahead_price_ch": "y", "date": "ds"})
 
 # Set how long should be forecasted
 HORIZON_SIZE = 38
 
-
 def objective(trial):
     # Define the hyperparameters to tune
-    forecast_window = HORIZON_SIZE
     lookback_window = trial.suggest_int("lookback_window", 24, 2 * 7 * 24)
     learning_rate = trial.suggest_loguniform("learning_rate", 1e-5, 1e-1)
     batch_size = trial.suggest_categorical("batch_size", [4, 8, 16, 32, 64, 128, 256])
-
-    # Create the model
+    
     model = NHITS(
-        h=38,
+        h=HORIZON_SIZE,
         input_size=lookback_window,
         loss=MSE(),
         scaler_type="standard",
         max_steps=500,
         val_check_steps=10,
-        early_stop_patience_steps=4,
+        early_stop_patience_steps=3,
         learning_rate=learning_rate,
         batch_size=batch_size,
         futr_exog_list=df_train.drop(columns=["y", "unique_id", "ds"]).columns.tolist(),
@@ -247,19 +261,19 @@ def objective(trial):
 
     # Create neuralforecast object
     nf = NeuralForecast(models=[model], freq="h")
-    nf.fit(df=df_train, val_size=VALIDATION_SIZE)
+    nf.fit(df=df_train, val_size=int(0.2*df_train.shape[0]))
 
     # Make predictions on the validation set and get validation loss
     val_pred_dfs = []
-    df_val = df_train.tail(VALIDATION_SIZE)
-    pred_timestamps = df_val.query("ds.dt.hour == 10")["ds"]
-
+    pred_timestamps = df_val.query("ds.dt.hour == 10")["ds"].reset_index(drop=True)
+    ts = pred_timestamps[10]
+    
     for ts in pred_timestamps:
         start_ts = ts - pd.Timedelta(lookback_window, unit="hours")
-        end_ts = ts
+        end_ts = ts + pd.Timedelta(HORIZON_SIZE, unit="hours")
         pred_day = ts.date() + pd.Timedelta(1, unit="days")
-        df_pred = df_train.query("@start_ts <= ds <= @end_ts")
-
+        df_pred = df_val.query("@start_ts <= ds <= @end_ts").reset_index(drop=True)
+        
         y_hat = nf.predict(futr_df=df_pred).reset_index()
         y_hat = (
             y_hat.query("ds.dt.date == @pred_day")
@@ -281,7 +295,7 @@ def objective(trial):
     return val_loss
 
 # Set three months aside for validation
-VALIDATION_SIZE = 92 * 24
+VALIDATION_SIZE = int(92 * 24)
 
 # Create the study object and optimize the objective function
 study = optuna.create_study(direction="minimize")
