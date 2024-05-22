70210969cdbc1158962c825709c9e3e31684631b
titoqu <tquadri@student.ethz.ch>
2024-05-22 10:01:48 +0200

    all

--- 05 Final Presentation/Predictive Model Evaluation Transmission Prices.R
+++ 05 Final Presentation/Predictive Model Evaluation Transmission Prices.R
@@ -8,9 +8,13 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()[[2]]))
 # "target": name the target
 # "data_type": name the data type (theoretical, predictive)
 
+cut <- as.POSIXct("2024-01-31 00:00:00", tz = "UTC")
+
+
 preds = bind_rows(
   # Baselines  -----------------------------------------
   read_csv("../02 Task 2 - Transmission Price/1 - BASELINE - Prediction - CHDE/holdout_predictions.csv",) |> 
+    filter(date<cut)|>
     transmute(
       date, .pred, actual,
       model = "Baseline",
@@ -18,6 +22,7 @@ preds = bind_rows(
       data_type = "Predictive"
     ),
   read_csv("../02 Task 2 - Transmission Price/1 - BASELINE - Prediction - CHDE/holdout_predictions.csv",) |> 
+    filter(date<cut)|>
     transmute(
       date, .pred, actual,
       model = "Baseline",
@@ -25,6 +30,7 @@ preds = bind_rows(
       data_type = "Theoretical"
     ),
   read_csv("../02 Task 2 - Transmission Price/1 - BASELINE - Prediction - DECH/holdout_predictions.csv",) |> 
+    filter(date<cut)|>
     transmute(
       date, .pred, actual,
       model = "Baseline",
@@ -32,6 +38,7 @@ preds = bind_rows(
       data_type = "Predictive"
     ),
   read_csv("../02 Task 2 - Transmission Price/1 - BASELINE - Prediction - DECH/holdout_predictions.csv",) |> 
+    filter(date<cut)|>
     transmute(
       date, .pred, actual,
       model = "Baseline",
@@ -41,6 +48,7 @@ preds = bind_rows(
   
   ## FastTS models -----------------------------------------
   read_csv("../02 Task 2 - Transmission Price/1 - fastTS - Prediction - CHDE - Data Predictive/holdout_predictions.csv",) |> 
+    filter(date<cut)|>
     transmute(
       date = dmy_hm(date), .pred, actual,
       model = "fastTS",
@@ -48,6 +56,7 @@ preds = bind_rows(
       data_type = "Predictive"
     ),
   read_csv("../02 Task 2 - Transmission Price/1 - fastTS - Prediction - CHDE - Data Theoretical/holdout_predictions.csv",) |> 
+    filter(date<cut)|>
     transmute(
       date = dmy_hm(date), .pred, actual,
       model = "fastTS",
@@ -55,6 +64,7 @@ preds = bind_rows(
       data_type = "Theoretical"
     ),
   read_csv("../02 Task 2 - Transmission Price/1 - fastTS - Prediction - DECH - Data Predictive/holdout_predictions.csv",) |> 
+    filter(date<cut)|>
     transmute(
       date = dmy_hm(date), .pred, actual,
       model = "fastTS",
@@ -62,6 +72,7 @@ preds = bind_rows(
       data_type = "Predictive"
     ),
   read_csv("../02 Task 2 - Transmission Price/1 - fastTS - Prediction - DECH - Data Theoretical/holdout_predictions.csv",) |> 
+    filter(date<cut)|>
     transmute(
       date = dmy_hm(date), .pred, actual,
       model = "fastTS",
@@ -69,8 +80,81 @@ preds = bind_rows(
       data_type = "Theoretical"
     ),
   
+  ## ARIMA -----------------------------------------
+  read_csv("../02 Task 2 - Transmission Price/1 - ARIMAX - Prediction - CHDE - Data Predictive/holdout_predictions.csv",) |> 
+    filter(date<cut)|>
+    transmute(
+      date = dmy_hm(date), .pred, actual,
+      model = "ARIMA",
+      target = "auction_price_ch_de",
+      data_type = "Predictive"
+    ),
+  read_csv("../02 Task 2 - Transmission Price/1 - ARIMAX - Prediction - CHDE - Data Theoretical/holdout_predictions.csv",) |> 
+    filter(date<cut)|>
+    transmute(
+      date = dmy_hm(date), .pred, actual,
+      model = "ARIMA",
+      target = "auction_price_ch_de",
+      data_type = "Theoretical"
+    ),
+  read_csv("../02 Task 2 - Transmission Price/1 - ARIMAX - Prediction - DECH - Data Predictive/holdout_predictions.csv",) |> 
+    filter(date<cut)|>
+    transmute(
+      date = dmy_hm(date), .pred, actual,
+      model = "ARIMA",
+      target = "auction_price_de_ch",
+      data_type = "Predictive"
+    ),
+  read_csv("../02 Task 2 - Transmission Price/1 - ARIMAX - Prediction - DECH - Data Theoretical/holdout_predictions.csv",) |> 
+    filter(date<cut)|>
+    transmute(
+      date = dmy_hm(date), .pred, actual,
+      model = "ARIMA",
+      target = "auction_price_de_ch",
+      data_type = "Theoretical"
+    ),
+  
+  ## GARCH -----------------------------------------
+  read_csv("../02 Task 2 - Transmission Price/1 - GARCH - Prediction - CHDE - Data Predictive/holdout_predictions.csv",) |> 
+    filter(date<cut)|>
+    mutate(actual = acutal)|>
+    transmute(
+      date = dmy_hm(date), .pred, actual,
+      model = "GARCH",
+      target = "auction_price_ch_de",
+      data_type = "Predictive"
+    ),
+  read_csv("../02 Task 2 - Transmission Price/1 - GARCH - Prediction - CHDE - Data Theoretical/holdout_predictions.csv",) |> 
+    filter(date<cut)|>
+    mutate(actual = acutal)|>
+    transmute(
+      date = dmy_hm(date), .pred, actual,
+      model = "GARCH",
+      target = "auction_price_ch_de",
+      data_type = "Theoretical"
+    ),
+  read_csv("../02 Task 2 - Transmission Price/1 - GARCH - Prediction - DECH - Data Predictive/holdout_predictions.csv",) |> 
+    filter(date<cut)|>
+    mutate(actual = acutal)|>
+    transmute(
+      date = dmy_hm(date), .pred, actual,
+      model = "GARCH",
+      target = "auction_price_de_ch",
+      data_type = "Predictive"
+    ),
+  read_csv("../02 Task 2 - Transmission Price/1 - GARCH - Prediction - DECH - Data Theoretical/holdout_predictions.csv",) |> 
+    filter(date<cut)|>
+    mutate(actual = acutal)|>
+    transmute(
+      date = dmy_hm(date), .pred, actual,
+      model = "GARCH",
+      target = "auction_price_de_ch",
+      data_type = "Theoretical"
+    ),
+  
   # LightGBM  -----------------------------------------
   read_csv("../02 Task 2 - Transmission Price/1 - LGBM - Prediction - CHDE - Data Predictive/holdout_predictions.csv",) |> 
+    filter(date<cut)|>
     transmute(
       date, .pred, actual,
       model = "LightGBM",
@@ -78,6 +162,7 @@ preds = bind_rows(
       data_type = "Predictive"
     ),
   read_csv("../02 Task 2 - Transmission Price/1 - LGBM - Prediction - CHDE - Data Theoretical/holdout_predictions.csv",) |> 
+    filter(date<cut)|>
     transmute(
       date, .pred, actual,
       model = "LightGBM",
@@ -85,6 +170,7 @@ preds = bind_rows(
       data_type = "Theoretical"
     ),
   read_csv("../02 Task 2 - Transmission Price/1 - LGBM - Prediction - DECH - Data Predictive/holdout_predictions.csv",) |> 
+    filter(date<cut)|>
     transmute(
       date, .pred, actual,
       model = "LightGBM",
@@ -92,6 +178,7 @@ preds = bind_rows(
       data_type = "Predictive"
     ),
   read_csv("../02 Task 2 - Transmission Price/1 - LGBM - Prediction - DECH - Data Theoretical/holdout_predictions.csv",) |> 
+    filter(date<cut)|>
     transmute(
       date, .pred, actual,
       model = "LightGBM",
@@ -101,6 +188,7 @@ preds = bind_rows(
   
   # NBEATS  -----------------------------------------
   read_csv("../02 Task 2 - Transmission Price/1 - NBEATS - Prediction - CHDE - Data Predictive/holdout_predictions.csv",) |> 
+    filter(date<cut)|>
     transmute(
       date, .pred, actual,
       model = "NBEATS",
@@ -108,6 +196,7 @@ preds = bind_rows(
       data_type = "Predictive"
     ),
   read_csv("../02 Task 2 - Transmission Price/1 - NBEATS - Prediction - CHDE - Data Theoretical/holdout_predictions.csv",) |> 
+    filter(date<cut)|>
     transmute(
       date, .pred, actual,
       model = "NBEATS",
@@ -115,6 +204,7 @@ preds = bind_rows(
       data_type = "Theoretical"
     ),
   read_csv("../02 Task 2 - Transmission Price/1 - NBEATS - Prediction - DECH - Data Predictive/holdout_predictions.csv",) |> 
+    filter(date<cut)|>
     transmute(
       date, .pred, actual,
       model = "NBEATS",
@@ -122,6 +212,7 @@ preds = bind_rows(
       data_type = "Predictive"
     ),
   read_csv("../02 Task 2 - Transmission Price/1 - NBEATS - Prediction - DECH - Data Theoretical/holdout_predictions.csv",) |> 
+    filter(date<cut)|>
     transmute(
       date, .pred, actual,
       model = "NBEATS",
@@ -131,6 +222,7 @@ preds = bind_rows(
   
   # XGBoost  -----------------------------------------
   read_csv("../02 Task 2 - Transmission Price/1 - XGBoost - Prediction - CHDE - Data Predictive/holdout_predictions.csv",) |> 
+    filter(date<cut)|>
     transmute(
       date, .pred, actual,
       model = "XGBoost",
@@ -138,6 +230,7 @@ preds = bind_rows(
       data_type = "Predictive"
     ),
   read_csv("../02 Task 2 - Transmission Price/1 - XGBoost - Prediction - CHDE - Data Theoretical/holdout_predictions.csv",) |> 
+    filter(date<cut)|>
     transmute(
       date, .pred, actual,
       model = "XGBoost",
@@ -145,6 +238,7 @@ preds = bind_rows(
       data_type = "Theoretical"
     ),
   read_csv("../02 Task 2 - Transmission Price/1 - XGBoost - Prediction - DECH - Data Predictive/holdout_predictions.csv",) |> 
+    filter(date<cut)|>
     transmute(
       date, .pred, actual,
       model = "XGBoost",
@@ -152,6 +246,7 @@ preds = bind_rows(
       data_type = "Predictive"
     ),
   read_csv("../02 Task 2 - Transmission Price/1 - XGBoost - Prediction - DECH - Data Theoretical/holdout_predictions.csv",) |> 
+    filter(date<cut)|>
     transmute(
       date, .pred, actual,
       model = "XGBoost",
@@ -167,16 +262,19 @@ preds = preds |>
   mutate(.pred = ifelse(.pred < 0, 0, .pred))
 
 
+
 ## Metrics ----
 eval_metrics = metric_set(rmse, mae, rsq)
 
-preds |> 
+preds<- preds |> 
   group_by(target, data_type, model) |>
   eval_metrics(truth = actual, estimate = .pred) |> 
   unique() |> 
   select(-c(.estimator)) |> 
   pivot_wider(names_from = .metric, values_from = .estimate) |> 
   arrange(data_type, target, rmse)
+
+write.csv(preds, "auctionPrices_metrics.csv")
 
 preds |> 
   group_by(target, data_type, model) |> 
@@ -185,6 +283,8 @@ preds |>
   ggplot(aes(target, .estimate, fill = model)) +
   geom_col(position = "dodge") +
   facet_wrap( ~ data_type + .metric, scales = "free")
+
+
 
 ## Time Series ----
 preds |> 
